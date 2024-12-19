2d832a351e65bd5cda827a0a0a4dbf7d8577de1a
Elizavetaa19 <klimova.liza.2005@gmail.com>
2024-12-19 20:49:56 +0300

    Add files via upload

---
+++ Лаба 6/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import TicTacToe from './components/TicTacToe/TicTacToe';
+
+function App() {
+    return (
+        <div>
+            <h1>Tic Tac Toe</h1>
+            <TicTacToe />
+        </div>
+    );
+}
+
+export default App;
