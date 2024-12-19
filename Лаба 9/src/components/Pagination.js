5a672f8b2459323e64022c10eaded16fdfd71fd3
Elizavetaa19 <klimova.liza.2005@gmail.com>
2024-12-20 00:32:20 +0300

    Add files via upload

---
+++ Лаба 9/src/components/Pagination.js
@@ -0,0 +1,33 @@
+// src/components/Pagination.js
+import React from 'react';
+import './Pagination.css'; // Импортируем стили для пагинации
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const handlePageChange = (page) => {
+        if (page > 0 && page <= totalPages) {
+            onPageChange(page);
+        }
+    };
+
+    return (
+        <div className="pagination">
+            <button 
+                className="pagination-button" 
+                onClick={() => handlePageChange(currentPage - 1)} 
+                disabled={currentPage === 1}
+            >
+                Назад
+            </button>
+            <span className="pagination-info">Страница {currentPage} из {totalPages}</span>
+            <button 
+                className="pagination-button" 
+                onClick={() => handlePageChange(currentPage + 1)} 
+                disabled={currentPage === totalPages}
+            >
+                Вперед
+            </button>
+        </div>
+    );
+};
+
+export default Pagination;
