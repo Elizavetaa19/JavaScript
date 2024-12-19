930fa3b1533bc0c8f4912c8852fb8304fa0ad35c
Elizavetaa19 <klimova.liza.2005@gmail.com>
2024-12-19 20:53:49 +0300

    Add files via upload

---
+++ Лаба 5/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import UserCard from './components/UserCard/UserCard';
+
+function App() {
+  return (
+    <div className="App">
+     <UserCard/>
+    </div>
+  );
+}
+
+
+
+export default App;
+
+  
