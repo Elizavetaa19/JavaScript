93fe9ce73f8a4bff4f7b646054025bb3acc2dcbd
Elizavetaa19 <klimova.liza.2005@gmail.com>
2024-12-19 20:52:24 +0300

    Delete Лаба 5 directory

--- Лаба 5/src/components/TestString/TestString.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestString from './TestString';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TestString />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
